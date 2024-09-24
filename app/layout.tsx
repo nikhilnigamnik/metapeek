3261f7936c4b76140a515a34f699b12cb4f3e5d6
nikhilnigamnik <nikhilnigamnik@gmail.com>
2024-09-24 14:03:42 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Metapeek",
+  description:
+    "Preview how your website will appear on social media platforms like Twitter, WhatsApp, and Discord. Easily visualize metadata and enhance link sharing across networks.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
